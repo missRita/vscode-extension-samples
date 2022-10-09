34e4e4b500b621ac6a1659cae5f4aa8b9c72d871
ssb <ssb@datakrat.ru>
2022-10-09 22:41:20 +0500

    последние наработки

--- tree-view-sample/src/extension.ts
+++ tree-view-sample/src/extension.ts
@@ -17,10 +17,23 @@ export function activate(context: vscode.ExtensionContext) {
 	const nodeDependenciesProvider = new DepNodeProvider(rootPath);
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => 
+	{
+		const uri = vscode.Uri.file(moduleName);
+		vscode.workspace.openTextDocument(uri).then(doc => {
+			const ed = vscode.window.showTextDocument(doc);
+		}
+		);
+		
+		//vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`));
+	});
+	vscode.commands.registerCommand('nodeDependencies.addEntry', 
+	() => 
+	vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => 
+	vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => 
+	vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
 
 	const jsonOutlineProvider = new JsonOutlineProvider(context);
 	vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
--- tree-view-sample/src/nodeDependencies.ts
+++ tree-view-sample/src/nodeDependencies.ts
@@ -19,23 +19,18 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	}
 
 // TODO
-path => resource uri
-verseion => bookmap flag
-tooltip хз
-кнопку обновления оставить на будущее
-остальные команды убрать
-команду открытия сделать на активейшене
-иконки
+/*
+кнопку обновления оставить на будущее 
+12345678901234567890123456789012.. 40->|
 описать структуру json
-уточнить про вложенность гк-карта-карта-топик (гк-топик может быть?) если да то признак для иконки: раздел/карта/топик. оринтируюсь пока на схему: ГК-раздел-карта-топик
-
+ */
 	getChildren(element?: Dependency): Thenable<Dependency[]> {
 		if(element === undefined)
 		{
-			const b = this.Eprst(undefined, '');
+			const b = this.Eprst2(undefined);
 			return Promise.resolve(b);
 		}
-		const a = this.Eprst(element.label, element.description as string);
+		const a = this.Eprst2(element.label);
 		return Promise.resolve(a);
 
 /* 
@@ -44,6 +39,63 @@ tooltip хз
 			return Promise.resolve([]);
 		}
 */
+	}
+
+	private Eprst2(name: string) : Dependency[]
+	{
+		const result : Dependency[] = [];
+		
+		// список дочерних
+		let list; 
+		let iconType = 0;
+		const lowlevel = false;
+
+		if(name === undefined){
+			// первый уровень - ГК
+			list = tree3.elems;
+			iconType = 0;
+		}
+		
+		else {
+			go(tree3.elems);
+
+			// на вход список elems любого уровня
+			function go(somelist: any): void {
+
+				// может быть пустой или вообще не добавлен
+				if(somelist === undefined) return;
+
+				// пробегаемся по каждому в списке
+				for (let i = 0; i < somelist.length; i++) {
+					const el = somelist[i];
+					if (el.name === name) {
+						list =  el.elems;
+					}
+					else {
+						go(el.elems);
+					}
+
+				}
+			}
+		}
+
+		// список дочерних не нашли
+		if(list === undefined) return result;
+
+		// заполняем листья узла
+		list.forEach( (element : {name: string; path: string, type: number }) => {
+			const dep = new Dependency(element.name, element.path, element.type,
+				element.type === 3 ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
+				{
+					command: 'extension.openPackageOnNpm',
+					title: '',
+					arguments: [element.path]
+				});
+			result.push(dep);
+		});
+
+		return result;
+
 	}
 
 	private Eprst(name: string, description: string) : Dependency[]
@@ -55,44 +107,51 @@ tooltip хз
 		let iconType = 0;
 		let lowlevel = false;
 
-		//if(tree3.bookmap.name === name){
+		
 		if(name === undefined){
-			//list = tree3.bookmap.elems;
-			//iconType = 0;
-			const dep = new Dependency('bookmap', '', iconType, vscode.TreeItemCollapsibleState.Collapsed);
-			dep.description = '777';
-			result.push(dep);
-			return result;
-		}
-		if(description === '777') 
-		{
-			list = tree3.bookmap.elems;
+			// первый уровень - ГК
+			list = tree3.elems;
 			iconType = 0;
 		}
 		
 		if(list === undefined)
 		{
-			// ищем на втором уровне - названия чаптеров
-			for (let index = 0; index < tree3.bookmap.elems.length; index++) {
-				const chapter = tree3.bookmap.elems[index];
-				if(chapter.name === name )
+			// ищем на втором уровне - названия карт
+			for (let index = 0; index < tree3.elems.length; index++) {
+				const bookmap = tree3.elems[index];
+				if(bookmap.name === name )
 				{
-					list = chapter.elems;
+					list = bookmap.elems;
 					iconType = 1;
 					break;
 				}
 				
 			}
 		}
+
+		if(list === undefined)
+		{
+			// ищем на третьем уровне - названия чаптеров
+			for (let index = 0; index < tree3.elems[0].elems.length; index++) {
+				const chapter = tree3.elems[0].elems[index];
+				if(chapter.name === name )
+				{
+					list = chapter.elems;
+					iconType = 2;
+					break;
+				}
+				
+			}
+		}
 		if (list === undefined) {
-			// ищем на втором уровне - названия карт
-			for (let index = 0; index < tree3.bookmap.elems.length; index++) {
-				const chapter = tree3.bookmap.elems[index];
+			// ищем на четвертом уровне - названия карт
+			for (let index = 0; index < tree3.elems[0].elems.length; index++) {
+				const chapter = tree3.elems[0].elems[index];
 				for (let index2 = 0; index2 < chapter.elems.length; index2++) {
 					const map = chapter.elems[index2];
 					if (map.name === name) {
 						list = map.elems;
-						iconType = 2;
+						iconType = 3;
 						break;
 					}
 				}
@@ -103,25 +162,13 @@ tooltip хз
 			}
 		}
 
-/* 		if (list === undefined) {
-			// ищем на третьем уровне - конкретный топик
-			for (let index = 0; index < tree3.bookmap.elems.length; index++) {
-				const chapter = tree3.bookmap.elems[index];
-				for (let index2 = 0; index2 < chapter.elems.length; index2++) {
-					const map = chapter.elems[index2];
-					for (let index3 = 0; index3 < map.elems.length; index3++) {
-						if (map[index3].name === name) {
-						//
-						}
-					}
-				}
-			}
-		}  */
-
+		// список дочерних не нашли
 		if(list === undefined) return result;
 
+		// заполняем листья узла
 		list.forEach( (element : {name: string; path: string }) => {
-			const dep = new Dependency(element.name, element.path, iconType, lowlevel ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
+			const dep = new Dependency(element.name, element.path, iconType,
+				lowlevel ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
 				{
 					command: 'extension.openPackageOnNpm',
 					title: '',
@@ -141,111 +188,165 @@ export class Dependency extends vscode.TreeItem {
 	constructor(
 		public readonly label: string,
 		private readonly version: string,
-		private iconW : number,
+		private type : number,
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
 		public readonly command?: vscode.Command
 	) {
 		super(label, collapsibleState);
 
-		this.tooltip = `${this.version}`;
-		this.iconW = iconW;
+		this.tooltip = this.version;
+		this.type = type;
 
-		if(iconW === 0 ){
-			this.iconPath = {
-				light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-				dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-			};
+		let iconName : string;
+		switch (type) {
+			case 0:
+				iconName = 'dependency';
+				break;
+			case 1:
+				iconName = 'folder';
+				break;
+
+			case 2:
+				iconName = 'number';
+				break;
+
+			case 3:
+				iconName = 'document';
+				break;
+
+			default:
+				break;
 		}
-		if(iconW === 1 ){
-			this.iconPath = {
-				light: path.join(__filename, '..', '..', 'resources', 'light', 'folder.svg'),
-				dark: path.join(__filename, '..', '..', 'resources', 'dark', 'folder.svg')
+
+		this.iconPath = {
+				light: path.join(__filename, '..', '..', 'resources', 'light', `${iconName}.svg`),
+				dark: path.join(__filename, '..', '..', 'resources', 'dark', `${iconName}.svg`)
 			};
-		}
-		if(iconW === 2 ){
-			this.iconPath = {
-				light: path.join(__filename, '..', '..', 'resources', 'light', 'number.svg'),
-				dark: path.join(__filename, '..', '..', 'resources', 'dark', 'number.svg')
-			};
-		}
 	}
 
 	contextValue = 'dependency';
 }
 
 const tree3 = {
-	bookmap: {
-		name: 'ar.ditamap',
-		path: '',
-		elems: [
-			{
-				name: 'ch1',
-				path: '',
-				elems: [
-					{
-						name: 'map1',
-						path: 'mappath1',
-						elems: [
-							{
-								name: 'topic2',
-								path: 'topicpath2',
-								elems: []
-							},
-							{
-								name: 'topic2',
-								path: 'topicpath2'
-							},
-							{
-								name: 'topic3',
-								path: 'topicpath3'
-							}
-						]
-					}
-				]
-			},
-			{
-				name: 'ch2',
-				elems: [
-					{
-						name: 'map2',
-						path: 'mappath2',
-						elems: [
-							{
-								name: 'topic4',
-								path: 'topicpath4'
-							}
-						]
-					},
-					{
-						name: 'map3',
-						path: 'mappath3',
-						elems: [
-							{
-								name: 'topic5',
-								path: 'topicpath5'
-							},
-							{
-								name: 'topic6',
-								path: 'topicpath6'
-							}
-						]
-					}
-				]
-			},
-			{
-				name: 'ch3',
-				elems: [
-					{
-						name: 'map4',
-						path: 'mappath4',
-						elems: []
-					},
-				]
-			},
-			{
-				name: 'ch4',
-				elems: []
-			},
-		]
-	}
+	elems: [
+		{
+			name: 'ar.ditamap',
+			path: '',
+			type: 0,
+			elems: [
+				{
+					name: 'ch1',
+					path: '',
+					type: 1,
+					elems: [
+						{
+							name: 'map1',
+							path: 'mappath1',
+							type: 2,
+							elems: [
+								{
+									name: 'topic1',
+									path: 'd:\\DITA2\\TemplatePD\\products\\AR\\_AR-GlosaryList.ditamap',
+									elems: [],
+									type: 3,
+								},
+								{
+									name: 'topic2',
+									path: 'topicpath2',
+									type: 3,
+								},
+								{
+									name: 'topic3',
+									path: 'topicpath3',
+									type: 3,
+								}
+							]
+						},
+						{
+							name: 'ind_topic1',
+							path : 'd:\\DITA2\\TemplatePD\\products\\P5\\topic\\p5-ooutvmib-treb.dita',
+							type: 3,
+						}
+					]
+				},
+				{
+					name: 'ch2',
+					type: 1,
+					elems: [
+						{
+							name: 'map2',
+							path: 'mappath2',
+							type: 2,
+							elems: [
+								{
+									name: 'topic4',
+									path: 'topicpath4',
+									type: 3,
+								}
+							]
+						},
+						{
+							name: 'map3',
+							path: 'mappath3',
+							type: 2,
+							elems: [
+								{
+									name: 'topic5',
+									path: 'topicpath5',
+									type: 3,
+								},
+								{
+									name: 'topic6',
+									path: 'topicpath6',
+									type: 3,
+								}
+							]
+						}
+					]
+				},
+				{
+					name: 'ch3',
+					type: 1,
+					elems: [
+						{
+							name: 'map4',
+							path: 'mappath4',
+							type: 2,
+							elems: []
+						},
+					]
+				},
+				{
+					name: 'ch4',
+					type: 1,
+					elems: []
+				},
+				{
+					name: 'ch5',
+					type: 1,
+					elems: [
+						{
+							name: 'map5',
+							path: 'mappath5',
+							type: 2,
+							elems: [
+								{
+									name: 'map6',
+									path: 'mappath6',
+									type: 2,
+									elems: [
+										{
+											name: 'topic7',
+											path: 'topicpath7',
+											type: 3,
+										}
+									]
+								}
+							]
+						},
+					]
+				},
+			]
+		}
+	]
 };

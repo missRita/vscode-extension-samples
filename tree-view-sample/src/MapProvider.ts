9d692f284c37a36dd9c8b64f40f7ed6aa53527f4
ssb <ssb@datakrat.ru>
2022-11-24 18:43:21 +0500

    модификация под структуру

--- tree-view-sample/src/MapProvider.ts
+++ tree-view-sample/src/MapProvider.ts
@@ -23,40 +23,40 @@ export class MapProvider implements vscode.TreeDataProvider<Dependency> {
 	getChildren(element?: Dependency): Thenable<Dependency[]> {
 		if(element === undefined)
 		{
-			const b = this.TreeGetChilds(undefined);
+			const b = this.TreeGetChilds(undefined, 1);
 			return Promise.resolve(b);
 		}
-		const a = this.TreeGetChilds(element.gid);
+		const a = this.TreeGetChilds(element.gid, element.type);
 		return Promise.resolve(a);
-
-/* 
-		if (!this.workspaceRoot) {
-			vscode.window.showInformationMessage('No dependency in empty workspace');
-			return Promise.resolve([]);
-		}
-*/
 	}
 
-	private TreeGetChilds(name: string) : Dependency[]
+	private TreeGetChilds(id: string, type: number) : Dependency[]
 	{
 		const result : Dependency[] = [];
 		
 		// список дочерних
-		let list; 
-		let iconType = 0;
-		let id;
-		const lowlevel = false;
-
+		let list;
 		
-		if(name === undefined){
+		if(id === undefined){
 			// первый уровень - ГК
-			list = tree3.elems;
-			iconType = 0;
+			let title = tree3.navTitle;
+			if(title === '') title = tree3.fileName; // TODO short
+
+			const dep = new Dependency(title, tree3.fileName, tree3.type, tree3.id,
+				tree3.type === 2 ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
+				{
+					command: 'dita-vs-code.openFile',
+					title: '',
+					arguments: [tree3.fileName]
+				});
+			result.push(dep);
+			return result;
 		}
 		
 		else 
 		{
-			go(tree3.elems);
+			if(type !== 0) go(tree3.elems);
+			else list = tree3.elems;
 
 			// на вход список elems любого уровня, внутри заполняется list - список его детей
 			function go(somelist: any): void {
@@ -67,7 +67,7 @@ export class MapProvider implements vscode.TreeDataProvider<Dependency> {
 				// пробегаемся по каждому в списке
 				for (let i = 0; i < somelist.length; i++) {
 					const el = somelist[i];
-					if (el.id === name) {
+					if (el.id === id) {
 						list =  el.elems;
 					}
 					else {
@@ -82,13 +82,13 @@ export class MapProvider implements vscode.TreeDataProvider<Dependency> {
 		if(list === undefined) return result;
 
 		// заполняем листья узла
-		list.forEach( (element : {name: string; path: string, type: number, id: string }) => {
-			const dep = new Dependency(element.name, element.path, element.type, element.id,
+		list.forEach( (element : {navTitle: string; fileName: string, type: number, id: string }) => {
+			const dep = new Dependency(element.navTitle, element.fileName, element.type, element.id,
 				element.type === 3 ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
 				{
 					command: 'dita-vs-code.openFile',
 					title: '',
-					arguments: [element.path]
+					arguments: [element.fileName]
 				});
 			result.push(dep);
 		});
@@ -102,7 +102,7 @@ export class Dependency extends vscode.TreeItem {
 	constructor(
 		public readonly label: string,
 		private readonly version: string,
-		private type : number,
+		public type : number,
 		public gid : string,
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
 		public readonly command?: vscode.Command,
@@ -115,14 +115,14 @@ export class Dependency extends vscode.TreeItem {
 		let iconName : string;
 		switch (type) {
 			case 0:
-				iconName = 'dependency';
+				iconName = 'boolean';
 				break;
 			case 1:
-				iconName = 'folder';
+				iconName = 'string';
 				break;
 
 			case 2:
-				iconName = 'number';
+				iconName = 'dependency';
 				break;
 
 			case 3:
@@ -145,113 +145,112 @@ export class Dependency extends vscode.TreeItem {
 }
 
 const tree3 = {
-	elems: [
-		{
-			name: 'ЦЕНТРАЛЬНЫЙ БАНК РОССИЙСКОЙ ФЕДЕРАЦИИ',
-			path: '',
+
+			navTitle: 'ЦЕНТРАЛЬНЫЙ БАНК РОССИЙСКОЙ ФЕДЕРАЦИИ',
+			fileName: '',
 			type: 0,
 			id: '1',
 			elems: [
 				{
-					name: 'frontmatter',
-					path: 'mappath1',
+					navTitle: 'frontmatter',
+					fileName: 'mapfilename1',
 					type: 1,
 					id: '2',
 					elems: [
 						{
-							name: 'Глоссарий',
-							path: 'mappath2',
+							navTitle: 'Глоссарий',
+							fileName: 'mapfilename2',
 							type: 2,
 							id: '3',
 							elems: [
 								{
-									name: 'Архитектурное решение',
-									path: 'd:\\DITA2\\TemplatePD\\products\\AR\\_AR-GlosaryList.ditamap',
+									navTitle: 'Архитектурное решение',
+									fileName: 'd:\\DITA2\\TemplatePD\\products\\AR\\_AR-GlosaryList.ditamap',
 									elems: [],
 									type: 3,
 									id: '4',
 								},
 								{
-									name: 'Автоматизированная система',
-									path: 'topicpath2',
+									navTitle: 'Автоматизированная система',
+									fileName: 'topicfilename2',
 									type: 3,
 									id: '5',
 								},
 								{
-									name: 'Информационные технологии',
-									path: 'topicpath3',
+									navTitle: 'Информационные технологии',
+									fileName: 'topicfilename3',
 									type: 3,
 									id: '6',
 								},
 								{
-									name: 'Информационно-телекоммуникационная система',
-									path: 'topicpath4',
+									navTitle: 'Информационно-телекоммуникационная система',
+									fileName: 'topicfilename4',
 									type: 3,
 									id: '7',
 								}
 							]
 						},
 						{
-							name: 'Термины и определения',
-							path: 'mappath3',
+							navTitle: 'Термины и определения',
+							fileName: 'mapfilename3',
 							type: 2,
 							id: '8',
 							elems: [
 								{
-									name: 'Архитектурное решение',
-									path: 'topicpath5',
+									navTitle: 'Архитектурное решение',
+									fileName: 'topicfilename5',
 									elems: [],
 									type: 3,
 									id: '9',
 								},
 								{
-									name: 'Автоматизированная система',
-									path: 'topicpath6',
+									navTitle: 'Автоматизированная система',
+									fileName: 'topicfilename6',
 									type: 3,
 									id: '10',
 								},
 								{
-									name: 'Условно-графические обозначения',
-									path: 'topicpath7',
+									navTitle: 'Условно-графические обозначения',
+									fileName: 'topicfilename7',
 									type: 3,
 									id: '11',
 								}
 							]
 						},
 						{
-							name: 'predislovie.dita',
-							path : 'd:\\DITA2\\TemplatePD\\products\\P5\\topic\\p5-ooutvmib-treb.dita',
+							navTitle: 'predislovie.dita',
+							fileName : 'd:\\DITA2\\TemplatePD\\products\\P5\\topic\\p5-ooutvmib-treb.dita',
 							type: 3,
 							id: '12',
 						}
 					]
 				},
 				{
-					name: 'Общие сведения',
+					navTitle: 'Общие сведения',
 					type: 1,
 					id: '13',
 					elems: [
 						{
-							name: 'Общие сведения2',
-							path: 'mappath4',
+							navTitle: 'Общие сведения2',
+							fileName: 'mapfilename4',
 							type: 2,
 							id: '14',
 							elems: [
 								{
-									name: 'Постановка задачи',
-									path: 'topicpath8',
+									navTitle: 'Постановка задачи',
+									fileName: 'topicfilename8',
 									type: 3,
 									id: '15',
 								},
 								{
-									name: 'Назначение ИТ-решения',
-									path: 'topicpath9',
+									navTitle: 'Назначение ИТ-решения',
+									fileName: 'topicfilename9',
 									type: 3,
 									id: '16',
 								},
 								{
-									name: 'Анализ функциональных требований',
-									path: 'topicpath10',
+									navTitle: 'Анализ функциональных требований',
+									fileName: 'topicfilename10',
 									type: 3,
 									id: '17',
 								}
@@ -260,25 +259,25 @@ const tree3 = {
 					]
 				},
 				{
-					name: 'Верхнеуровневая архитектура ИТ-решения',
+					navTitle: 'Верхнеуровневая архитектура ИТ-решения',
 					type: 1,
 					id: '18',
 					elems: [
 						{
-							name: 'Верхнеуровневая архитектура ИТ-решения2',
-							path: 'mappath5',
+							navTitle: 'Верхнеуровневая архитектура ИТ-решения2',
+							fileName: 'mapfilename5',
 							type: 2,
 							id: '19',
 							elems: [
 								{
-									name: 'Целевая архитектура ИТ-решения',
-									path: 'topicpath11',
+									navTitle: 'Целевая архитектура ИТ-решения',
+									fileName: 'topicfilename11',
 									type: 3,
 									id: '20',
 								},
 								{
-									name: 'Транзитная архитектура ИТ-решения',
-									path: 'topicpath12',
+									navTitle: 'Транзитная архитектура ИТ-решения',
+									fileName: 'topicfilename12',
 									type: 3,
 									id: '21',
 								}
@@ -287,13 +286,13 @@ const tree3 = {
 					]
 				},
 				{
-					name: 'Перечень вопросов, требующих дополнительной проработки2',
+					navTitle: 'Перечень вопросов, требующих дополнительной проработки2',
 					type: 1,
 					id: '22',
 					elems: [
 						{
-							name: 'Перечень вопросов, требующих дополнительной проработки',
-							path: 'mappath6',
+							namnavTitlee: 'Перечень вопросов, требующих дополнительной проработки',
+							fileName: 'mapfilename6',
 							type: 2,
 							id: '23',
 							elems: []
@@ -301,33 +300,33 @@ const tree3 = {
 					]
 				},
 				{
-					name: 'appendices',
+					navTitle: 'appendices',
 					type: 1,
 					id: '24',
 					elems: [
 						{
-							name: 'appendix',
-							path: 'mappath7',
+							navTitle: 'appendix',
+							fileName: 'mapfilename7',
 							type: 2,
 							id: '25',
 							elems: [
 								{
-									name: 'Используемые в документе условно-графические обозначения',
-									path: 'mappath13',
+									navTitle: 'Используемые в документе условно-графические обозначения',
+									fileName: 'mapfilename13',
 									type: 3,
 									id: '26',
 								}
 							]
 						},
 						{
-							name: 'appendix2',
-							path: 'mappath8',
+							navTitle: 'appendix2',
+							fileName: 'mapfilename8',
 							type: 2,
 							id: '27',
 							elems: [
 								{
-									name: 'Дополнительная информация',
-									path: 'mappath14',
+									navTitle: 'Дополнительная информация',
+									fileName: 'mapfilename14',
 									type: 3,
 									id: '28'
 								}
@@ -336,6 +335,5 @@ const tree3 = {
 					]
 				},
 			]
-		}
-	]
+
 };
